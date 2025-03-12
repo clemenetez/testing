eae54c9020931a0b32c1113883db7d3923ce1bd9
vladyslav <vodovodo79@gmail.com>
2025-03-12 11:39:46 +0200

    add dir

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	SMTP_HOST     = "smtp.gmail.com"
+	SMTP_PORT     = 587
+	SMTP_USERNAME = "arz2564720@gmail.com" // Ваша Gmail-адреса
+	SMTP_PASSWORD = "lvsroaoctdfjjbcq"     // Пароль додатку з кроку 1.1
+)
